455a9b8d7e855947ff0f7a13f64ab81d0d8106a9
yao.li <yao.li@cootek.cn>
2018-05-22 11:58:12 +0800

    [feature] add new macro

--- Coolog/Classes/CoologMacros.h
+++ Coolog/Classes/CoologMacros.h
@@ -10,18 +10,29 @@
 #ifndef CoologMacros_h
 #define CoologMacros_h
 
-#define CLogMethod(type, atag, frmt, ...) \
+#define CLogWithType(type, atag, frmt, ...) \
 do {[[COLLogManager sharedInstance] logWithType:type tag:atag message:[NSString stringWithFormat:(frmt), ##__VA_ARGS__]];} while(0)
 
 #define CLogError(tag, frmt, ...) \
-CLogMethod(COLLogTypeError, tag, frmt, ##__VA_ARGS__)
+CLogWithType(COLLogTypeError, tag, frmt, ##__VA_ARGS__)
 #define CLogWarning(tag, frmt, ...) \
-CLogMethod(COLLogTypeWarning, tag, frmt, ##__VA_ARGS__)
+CLogWithType(COLLogTypeWarning, tag, frmt, ##__VA_ARGS__)
 #define CLogInfo(tag, frmt, ...) \
-CLogMethod(COLLogTypeInfo, tag, frmt, ##__VA_ARGS__)
+CLogWithType(COLLogTypeInfo, tag, frmt, ##__VA_ARGS__)
 #define CLogDefault(tag, frmt, ...) \
-CLogMethod(COLLogTypeDefault, tag, frmt, ##__VA_ARGS__)
+CLogWithType(COLLogTypeDefault, tag, frmt, ##__VA_ARGS__)
 #define CLogDebug(tag, frmt, ...) \
-CLogMethod(COLLogTypeDebug, tag, frmt, ##__VA_ARGS__)
+CLogWithType(COLLogTypeDebug, tag, frmt, ##__VA_ARGS__)
+
+#define CLogE(frmt, ...) \
+CLogError(nil, frmt, ##__VA_ARGS__)
+#define CLogW(frmt, ...) \
+CLogWarning(nil, frmt, ##__VA_ARGS__)
+#define CLogI(frmt, ...) \
+CLogInfo(nil, frmt, ##__VA_ARGS__)
+#define CLog(frmt, ...) \
+CLogDefault(nil, frmt, ##__VA_ARGS__)
+#define CLogD(frmt, ...) \
+CLogDebug(nil, frmt, ##__VA_ARGS__)
 
 #endif /* CoologMacros_h */
