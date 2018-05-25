fcf85ae6ffff3faef400b2a31ab2a45ee7e60f22
yao.li <yao.li@cootek.cn>
2018-05-25 18:39:12 +0800

    [feature] debug tool

--- CoologTool/vueDemo/src/App.vue
+++ CoologTool/vueDemo/src/App.vue
@@ -1,29 +1,32 @@
 <template>
-    <div id="app">
-        <button class="normalButton" v-on:click="openLogWebSocket" :disabled="isSocketOn">Start</button>
-        <button class="normalButton" v-on:click="closeLogWebSocket" :class="{hidden:!isSocketOn}">Stop</button>
-        <div class="listWrap">
-            <virtual-list ref="vsl" class="list" :variable="getVariableHeight" 
-            :size="78" 
-            :remain="7">
-              <item
-                v-for="(item, index) of items" 
-                :key="index"
-                :index="index"
-                :height="item.height"
-                :content="item.content"
-                :textStyle="item.textStyle"> 
-              </item>
-          </virtual-list>
-        </div>
+  <div id="app">
+    <button class="normalButton" v-on:click="openLogWebSocket" :disabled="isSocketOn">Start</button>
+    <button class="normalButton" v-on:click="closeLogWebSocket" :class="{hidden:!isSocketOn}">Stop</button>
+    <input type="text" class="search" v-model="searchText" v-on:keypress="searchInputKeypressed" placeholder="search">
+    <span style="color:grey" v-if="this.searchText.length>0" v-bind:style="searchSpanStyle">{{this.searchItems.length + ' logs contains "' + this.searchText + '"'}}</span>
+    <div class="listWrap" ref="listWrap">
+      <virtual-list ref="vsl" class="list" :start="startIndex" :variable="getVariableHeight" 
+      :size="78" 
+      :remain="7">
+        <item
+          v-for="(item, index) of filterItems" 
+          :key="index"
+          :index="index"
+          :height="item.height"
+          :content="item.content"
+          :textStyle="item.textStyle"> 
+        </item>
+      </virtual-list>
     </div>
+    <input type="text" class="filter" v-model="filterText" placeholder="Filter">
+  </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import virtualList from 'vue-virtual-scroll-list';
-import item from './item.vue';
 import VueNativeSock from 'vue-native-websocket'
+import virtualList from 'vue-virtual-scroll-list';
+import item from './Item.vue';
 
 let url = getQueryString("host");
 if (url != null) {
@@ -41,12 +44,34 @@ export default {
 
   data () {
     return {
-        items: new Array({height: 40, content: 'Coolog', textStyle:'color:#1E90FF;font-size:26px'}),
-        ws: null,
-        totalHeight: 40,
-        scrolling: false,
-        isSocketOn: false,
+      items: new Array({height: 40, content: 'Coolog', textStyle:'color:#1E90FF;font-size:26px'}),
+      searchItems: new Array(),
+
+      scrolling: false,
+      isSocketOn: false,
+      isSocketConnecting: false,
+      startIndex: 0,
+
+      filterText: '',
+      searchText: '',
+
+      searchSpanStyle: {
+        visibility: 'hidden',
+      }
     }
+  },
+
+  watch: {
+    searchText: function (val) {
+      this.searchSpanStyle.visibility = 'hidden';
+    }
+  },
+
+  computed: {
+    filterItems: function () {
+      var filterItems = this.items;
+      return filterItems.filter(this.itemFilter);
+    },
   },
 
   methods: {
@@ -55,22 +80,28 @@ export default {
       return target.height;
     },
 
-    openLogWebSocket() {
+    openLogWebSocket () {
+      if (this.isSocketConnecting) {
+        return;
+      }
+      this.isSocketConnecting = true;
       this.$connect();
-      this.$options.sockets.onopen = function() {
+      this.$options.sockets.onopen = function () {
         this.isSocketOn = true;
       };
-      this.$options.sockets.onclose = function() {
+      this.$options.sockets.onclose = function () {
         this.isSocketOn = false;
+        this.isSocketConnecting = false;
         this.closeLogWebSocket();
         this.removeWebSocketListener();
       };
-      this.$options.sockets.onerror = function(event) {
+      this.$options.sockets.onerror = function (event) {
         this.isSocketOn = false;
+        this.isSocketConnecting = false;
         this.closeLogWebSocket();
         this.removeWebSocketListener();
       };
-      this.$options.sockets.onmessage =  function(evt) {
+      this.$options.sockets.onmessage =  function (evt) {
         let rowHeight = 15 * Math.ceil((evt.data.length / 134)+1);
         let type = evt.data.match(/\[##TYPE:(\S*)##\]/)[1];
         var textColor = '#000000';
@@ -88,28 +119,48 @@ export default {
           content: evt.data,
           textStyle: 'color:'+textColor
         };
-        this.totalHeight += rowHeight;
         this.items.push(itm);
-
-        if (!this.scrolling) {
-          this.scrolling = true;
-          setTimeout(() => {
-            this.$refs.vsl.setScrollTop(this.totalHeight);
-            this.scrolling = false;
-          }, 100);
-        }
+        this.startIndex = this.items.length;
       };
     },
 
-    closeLogWebSocket() {
+    closeLogWebSocket () {
       this.$disconnect();
     },
 
-    removeWebSocketListener() {
+    removeWebSocketListener () {
       delete this.$options.sockets.onopen;
       delete this.$options.sockets.onclose;
       delete this.$options.sockets.onerror;
       delete this.$options.sockets.onmessage;
+    },
+
+    searchInputKeypressed (event) {
+      if (event.keyCode != 13) {
+        return;
+      }
+      this.searchSpanStyle.visibility = 'visible';
+      this.searchItems = this.getSearchItems();
+      if (this.searchItems.length > 0) {
+        this.startIndex = this.searchItems[0].index;
+        // var searchInnerHTML = this.$refs.listWrap.innerHTML;
+        // let re = new RegExp(this.searchText+'(?![^<>]*>)', 'g');
+        // this.$refs.listWrap.innerHTML = searchInnerHTML.replace(re, '<span style="color:#0f0;background-color:#ff0">'+this.searchText+'</span>');
+      }
+    },
+
+    getSearchItems () {
+      var searchItems = this.filterItems;
+      searchItems = searchItems.filter(this.itemSearcher);
+      return searchItems;
+    },
+
+    itemFilter (item) {
+      return (item.content.indexOf(this.filterText) != -1);
+    },
+
+    itemSearcher (item) {
+      return (item.content.indexOf(this.searchText) != -1);
     }
   }
 }
@@ -125,7 +176,7 @@ function getQueryString(name) {
     .normalButton {
       position: relative;
       font-family: Helvetica;
-      padding: 5px 10px 5px 10px;
+      margin: 5px 0px 5px 0px;
       font-size: 16pt;
     }
 
@@ -134,16 +185,28 @@ function getQueryString(name) {
     }
 
     .listWrap {
-        position: relative;
-        margin-top: 10px;
+      position: relative;
+      height: 560px;
     }
 
     .list {
-        background: #fff;
-        border-radius: 3px;
-        border: 1px solid #ddd;
-        -webkit-overflow-scrolling: touch;
-        overflow-scrolling: touch;
+      ackground: #fff;
+      border-radius: 3px;
+      border: 1px solid #ddd;
+      -webkit-overflow-scrolling: touch;
+      overflow-scrolling: touch;
+    }
+
+    .filter {
+      position: relative;
+      width: 150px;
+      font-size: 16pt;
+    }
+
+    .search {
+      position: relative;
+      width: 250px;
+      font-size: 14pt;
     }
 
 </style>
--- Example/Coolog/COLViewController.m
+++ Example/Coolog/COLViewController.m
@@ -39,23 +39,23 @@
 //        } withPrefix:@"LOG in main thread"];
 //    });
     
-//    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-//    queue.maxConcurrentOperationCount = 10;    
-//    for (int i=0; i<100; i++) {
-//        [queue addOperationWithBlock:^{
-//            CLogInfo(@"tag", @"%@", [message description]);
-//        }];
-//        [queue addOperationWithBlock:^{
-//            CLogError(@"tag", @"%@", [message description]);
-//        }];
-//        [queue addOperationWithBlock:^{
-//            CLogWarning(@"tag", @"%@", [message description]);
-//        }];
-//    }
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    queue.maxConcurrentOperationCount = 10;
+    for (int i=0; i<1; i++) {
+        [queue addOperationWithBlock:^{
+            CLogInfo(@"tag", @"%@", [message description]);
+        }];
+        [queue addOperationWithBlock:^{
+            CLogError(@"tag", @"%@", [message description]);
+        }];
+        [queue addOperationWithBlock:^{
+            CLogWarning(@"tag", @"%@", [message description]);
+        }];
+    }
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         [self logTimeTakenToRunBlock:^{
-            for (int i=0; i<10000; i++) {
+            for (int i=0; i<2; i++) {
 //                NSLog(@"tag %@ %@ %@", [message description], [NSThread currentThread], [NSDate date]);
                 CLogE(@"%@", @"test");
             }
